a3ab46382e68d3d2c33bda16a88076209356d839
abuza <ak3776@drexel.edu>
2024-04-21 04:58:50 -0400

    class selected

--- app/frontend/src/screens/studentScreen.jsx
+++ app/frontend/src/screens/studentScreen.jsx
@@ -1,6 +1,6 @@
 import './studentScreen.css';
 
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
 import Logo from '../assets/Logo.png';
@@ -11,6 +11,12 @@ import StudentAccordian from './studentAccordian.jsx';
 
 export default function StudentScreen() {
 
+    const [selectedClass, setSelectedClass] = useState('MATH 101');
+    const handleClassChange = (className) => {
+        setSelectedClass(className);
+        console.log('Selected Class: ', className);
+    }
+    
     const navigate = useNavigate();
 
     const handleAssignmentPage = () => {
@@ -51,22 +57,22 @@ export default function StudentScreen() {
     const lectureList = [
         {
             lectureNumber: '1.1',
-            lectureName: 'Introduction to the Course',
+            lectureName: 'CS164 Introduction to the Course',
             lectureContent: 'The chapter on "Programming Languages" in CS 164 Introduction to Programming Concepts explores the fundamental concepts and characteristics of various programming languages. It delves into the role of programming languages in software development and their impact on solving computational problems efficiently.'
         },
         {
             lectureNumber: '1.2',
-            lectureName: 'Second Thing',
+            lectureName: 'CS164 Second Thing',
             lectureContent: 'The chapter on "Programming Languages" in CS 164 Introduction to Programming Concepts explores the fundamental concepts and characteristics of various programming languages. It delves into the role of programming languages in software development and their impact on solving computational problems efficiently.'
         },
         {
             lectureNumber: '1.3',
-            lectureName: 'Third THing',
+            lectureName: 'CS164 Third THing',
             lectureContent: 'The chapter on "Programming Languages" in CS 164 Introduction to Programming Concepts explores the fundamental concepts and characteristics of various programming languages. It delves into the role of programming languages in software development and their impact on solving computational problems efficiently.'
         },
         {
             lectureNumber: '1.4',
-            lectureName: 'Fourth Thing',
+            lectureName: 'CS164 Fourth Thing',
             lectureContent: 'The chapter on "Programming Languages" in CS 164 Introduction to Programming Concepts explores the fundamental concepts and characteristics of various programming languages. It delves into the role of programming languages in software development and their impact on solving computational problems efficiently.'
         },
         {
@@ -75,6 +81,117 @@ export default function StudentScreen() {
             lectureContent: 'The chapter on "Programming Languages" in CS 164 Introduction to Programming Concepts explores the fundamental concepts and characteristics of various programming languages. It delves into the role of programming languages in software development and their impact on solving computational problems efficiently.'
         },
     ];
+
+    const lectureByClass = {
+        'MATH 101': [
+            {
+                lectureNumber: '1.1',
+                lectureName: 'MATH101 Introduction to the Course',
+                lectureContent: 'The chapter on "Mathematics" in MATH 101 Introduction to Mathematics explores the fundamental concepts and characteristics of various mathematical concepts. It delves into the role of mathematics in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.2',
+                lectureName: 'MATH101 Second Thing',
+                lectureContent: 'The chapter on "Mathematics" in MATH 101 Introduction to Mathematics explores the fundamental concepts and characteristics of various mathematical concepts. It delves into the role of mathematics in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.3',
+                lectureName: 'MATH101 Third THing',
+                lectureContent: 'The chapter on "Mathematics" in MATH 101 Introduction to Mathematics explores the fundamental concepts and characteristics of various mathematical concepts. It delves into the role of mathematics in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.4',
+                lectureName: 'MATH101 Fourth Thing',
+                lectureContent: 'The chapter on "Mathematics" in MATH 101 Introduction to Mathematics explores the fundamental concepts and characteristics of various mathematical concepts. It delves into the role of mathematics in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.5',
+                lectureName: 'Math 101 Fifth Thing',
+                lectureContent: 'The chapter on "Mathematics" in MATH 101 Introduction to Mathematics explores the fundamental concepts and characteristics of various mathematical concepts. It delves into the role of mathematics in software development and their impact on solving computational problems efficiently.'
+            },
+        ],
+        'CHEM 121': [
+            {
+                lectureNumber: '1.1',
+                lectureName: 'CHEM121 Introduction to the Course',
+                lectureContent: 'The chapter on "Chemistry" in CHEM 121 Introduction to Chemistry explores the fundamental concepts and characteristics of various chemical concepts. It delves into the role of chemistry in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.2',
+                lectureName: 'CHEM121 Second Thing',
+                lectureContent: 'The chapter on "Chemistry" in CHEM 121 Introduction to Chemistry explores the fundamental concepts and characteristics of various chemical concepts. It delves into the role of chemistry in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.3',
+                lectureName: 'CHEM121 Third THing',
+                lectureContent: 'The chapter on "Chemistry" in CHEM 121 Introduction to Chemistry explores the fundamental concepts and characteristics of various chemical concepts. It delves into the role of chemistry in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.4',
+                lectureName: 'CHEM121 Fourth Thing',
+                lectureContent: 'The chapter on "Chemistry" in CHEM 121 Introduction to Chemistry explores the fundamental concepts and characteristics of various chemical concepts. It delves into the role of chemistry in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.5',
+                lectureName: 'CHEM 121 Fifth Thing',
+                lectureContent: 'The chapter on "Chemistry" in CHEM 121 Introduction to Chemistry explores the fundamental concepts and characteristics of various chemical concepts. It delves into the role of chemistry in software development and their impact on solving computational problems efficiently.'
+            },
+        ],
+        'PHYS 101': [
+            {
+                lectureNumber: '1.1',
+                lectureName: 'PHYS101 Introduction to the Course',
+                lectureContent: 'The chapter on "Physics" in PHYS 101 Introduction to Physics explores the fundamental concepts and characteristics of various physical concepts. It delves into the role of physics in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.2',
+                lectureName: 'PHYS101 Second Thing',
+                lectureContent: 'The chapter on "Physics" in PHYS 101 Introduction to Physics explores the fundamental concepts and characteristics of various physical concepts. It delves into the role of physics in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.3',
+                lectureName: 'PHYS101 Third THing',
+                lectureContent: 'The chapter on "Physics" in PHYS 101 Introduction to Physics explores the fundamental concepts and characteristics of various physical concepts. It delves into the role of physics in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.4',
+                lectureName: 'PHYS101 Fourth Thing',
+                lectureContent: 'The chapter on "Physics" in PHYS 101 Introduction to Physics explores the fundamental concepts and characteristics of various physical concepts. It delves into the role of physics in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.5',
+                lectureName: 'PHYS 101 Fifth Thing',
+                lectureContent: 'The chapter on "Physics" in PHYS 101 Introduction to Physics explores the fundamental concepts and characteristics of various physical concepts. It delves into the role of physics in software development and their impact on solving computational problems efficiently.'
+            },
+        ],
+        'CS 164': [
+            {
+                lectureNumber: '1.1',
+                lectureName: 'CS164 Introduction to the Course',
+                lectureContent: 'The chapter on "Programming Languages" in CS 164 Introduction to Programming Concepts explores the fundamental concepts and characteristics of various programming languages. It delves into the role of programming languages in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.2',
+                lectureName: 'CS164 Second Thing',
+                lectureContent: 'The chapter on "Programming Languages" in CS 164 Introduction to Programming Concepts explores the fundamental concepts and characteristics of various programming languages. It delves into the role of programming languages in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.3',
+                lectureName: 'CS164 Third THing',
+                lectureContent: 'The chapter on "Programming Languages" in CS 164 Introduction to Programming Concepts explores the fundamental concepts and characteristics of various programming languages. It delves into the role of programming languages in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.4',
+                lectureName: 'CS164 Fourth Thing',
+                lectureContent: 'The chapter on "Programming Languages" in CS 164 Introduction to Programming Concepts explores the fundamental concepts and characteristics of various programming languages. It delves into the role of programming languages in software development and their impact on solving computational problems efficiently.'
+            },
+            {
+                lectureNumber: '1.5',
+                lectureName: 'CS 164 Fifth Thing',
+                lectureContent: 'The chapter on "Programming Languages" in CS 164 Introduction to Programming Concepts explores the fundamental concepts and characteristics of various programming languages. It delves into the role of programming languages in software development and their impact on solving computational problems efficiently.'
+            },
+        ],
+    }
 
     return (
         // <div>
@@ -105,7 +222,10 @@ export default function StudentScreen() {
                             </div>
                         </div> */}
                         {classList.map((classItem, index) => (
-                            <div className='class_1' key={index}>
+                            <div 
+                                className='class_1' 
+                                key={index}
+                                onClick={() => handleClassChange(classItem.className)}>
                                 <div className='classImg'>
                                     <img src={classItem.classImage} alt='Class Image' />
                                 </div>
@@ -120,7 +240,16 @@ export default function StudentScreen() {
                 <div className='right_pannel'>
                     <StudentNav studentInfo={studentInfo}/>
                     <div className='right_component_placeholder'>
-                        {lectureList.map((lectureItem, index) => (
+                        {/* {lectureList.map((lectureItem, index) => (
+                            <StudentAccordian 
+                                key={index}
+                                lectureNumber={lectureItem.lectureNumber}
+                                lectureName={lectureItem.lectureName}
+                                lectureContent={lectureItem.lectureContent}
+                            />
+                        ))} */}
+
+                        {selectedClass && lectureByClass[selectedClass]?.map((lectureItem, index) => (
                             <StudentAccordian 
                                 key={index}
                                 lectureNumber={lectureItem.lectureNumber}
