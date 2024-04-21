6eed779a493db742113a9abbda85d12d954fa9f1
abuza <ak3776@drexel.edu>
2024-04-20 22:01:13 -0400

    submit

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState,useEffect } from 'react'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import CustomEditor from './codeIDE/CustomEditor';
+import LoginScreen from './screens/loginScreen';
+import StudentScreen from './screens/studentScreen';
+import TeacherScreen from './screens/teacherScreen';
+import MakeModuleScreen from './screens/makeModuleScreen';
+import AssignmentScreen from './screens/assignmentScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/studentPage" element={<StudentScreen />} />
+        <Route path="/teacherPage" element={<TeacherScreen />} />
+        <Route path="/makeModule" element={<MakeModuleScreen />} />
+        <Route path="/assignmentPage" element={<AssignmentScreen />} />
+      </Routes>
+    </div>
+  );
+}
 
 
-const App = () => {
-    
-    return (<>
-    
-         <CustomEditor />
-      
-        </>
-    );
-};
 
 export default App;
